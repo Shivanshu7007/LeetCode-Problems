1670daa0521494f43a5ca7c3f09de02865969442
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2023-12-23 17:57:20 +0530

    Time: 11 ms (6.61%) | Memory: 42.1 MB (6.17%) - LeetSync

--- 1619-path-crossing/path-crossing.java
+++ 1619-path-crossing/path-crossing.java
@@ -1,36 +1,31 @@
 class Solution {
     public boolean isPathCrossing(String path) {
-        int x=0,y=0;
-        HashSet<Pair<Integer,Integer>> hs=new HashSet<>();
-        hs.add(new Pair<>(0,0));
-        for(int i=0;i<path.length();i++)
-        {
-            if(path.charAt(i)=='N')
-            {
+        int x = 0, y = 0;
+        HashSet<String> visited = new HashSet<>();
+        visited.add(constructKey(0, 0));
+        for (int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == 'N') {
                 y++;
-            }
-            else if(path.charAt(i)=='E')
-            {
+            } else if (path.charAt(i) == 'S') {
+                y--;
+            } else if (path.charAt(i) == 'E') {
                 x++;
-            }
-            else if(path.charAt(i)=='W')
-            {
+            } else if (path.charAt(i) == 'W') {
                 x--;
             }
-            else if(path.charAt(i)=='S')
-            {
-                y--;
-            }
-            System.out.println(hs);
-            if(!hs.contains(new Pair<>(x,y)))
-            {
-                hs.add(new Pair<>(x,y));
-            }
-            else
-            {
+
+            String key = constructKey(x, y);
+            System.out.println(key);
+            if (visited.contains(key)) {
                 return true;
             }
+            visited.add(key);
         }
+
         return false;
+    }
+
+    private String constructKey(int x, int y) {
+        return x + " " + y;
     }
 }
