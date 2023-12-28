c508bf15fb2a9dbb6b6a9e9d8715345109f55150
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2023-12-28 20:17:14 +0530

    Time: 48 ms (73.91%) | Memory: 42.9 MB (73.91%) - LeetSync

--- 1637-string-compression-ii/string-compression-ii.java
+++ 1637-string-compression-ii/string-compression-ii.java
@@ -1,30 +1,26 @@
 class Solution {
     public int getLengthOfOptimalCompression(String s, int k) {
         int n = s.length();
-        int[][] dp = new int[110][110];
-        for (int i = 0; i <= n; i++)
-            for (int j = 0; j <= n; j++)
-                dp[i][j] = 9999;
+        int[][] dp = new int[n + 1][k + 1]; 
+        for (int i = 1; i <= n; i++) { 
+            for (int j = 0; j <= k; j++) dp[i][j] = n; 
+        }
         dp[0][0] = 0;
-        
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                int cnt = 0, del = 0;
-                for (int l = i; l >= 1; l--) {
-                    if (s.charAt(l - 1) == s.charAt(i - 1)) 
-                        cnt++;
-                    else 
-                        del++;
+            char c = s.charAt(i - 1); 
+            for (int j = 0; j <= k; j++) {                
+                int cnt = 0;
+                int del = 0;
+                for (int l = i; l > 0; l--) {
+                    if (s.charAt(l - 1) == c) cnt++;
+                    else del++;
+                    if (j < del) break; 
+                    dp[i][j] = Math.min(dp[i][j], dp[l - 1][j - del] + (cnt == 1 ? 1 : cnt < 10 ? 2 : cnt < 100 ? 3: 4)); 
                     
-                    if (j - del >= 0) {
-                        dp[i][j] = Math.min(dp[i][j], dp[l - 1][j - del] + 1 + (cnt >= 100 ? 3 : cnt >= 10 ? 2 : cnt >= 2 ? 1 : 0));
-                    }
                 }
-                if (j > 0)
-                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]);
+                if (j > 0) dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]);
             }
         }
         return dp[n][k];
     }
 }
-
