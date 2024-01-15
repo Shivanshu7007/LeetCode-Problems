42567d0ca25c33ae11c7d99d9da672ed67b0b477
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2024-01-15 18:56:00 +0530

    Time: 1 ms (91.69%) | Memory: 40.5 MB (37.08%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -3,15 +3,12 @@ class Solution {
     dfs(n, 0, new boolean[n], new boolean[2 * n - 1], new boolean[2 * n - 1]);
     return ans;
   }
-
   private int ans = 0;
-
   private void dfs(int n, int i, boolean[] cols, boolean[] diag1, boolean[] diag2) {
     if (i == n) {
       ++ans;
       return;
     }
-
     for (int j = 0; j < cols.length; ++j) {
       if (cols[j] || diag1[i + j] || diag2[j - i + n - 1])
         continue;
