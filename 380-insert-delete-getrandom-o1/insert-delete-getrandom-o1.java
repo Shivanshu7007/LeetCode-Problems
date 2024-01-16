d82eaeb04e689fe43a1adfc1635528a84d698836
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2024-01-16 21:05:15 +0530

    Time: 23 ms (84.45%) | Memory: 93.4 MB (23.59%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -1,29 +1,51 @@
 class RandomizedSet {
-    private List<Integer> list;
+    HashMap<Integer, Integer> map;
+	List<Integer> list;
+	Random random;
 
     public RandomizedSet() {
-        list = new ArrayList<>();
+        map=new HashMap<>();
+        list=new ArrayList<>();
+        random=new Random();
     }
-
+    
     public boolean insert(int val) {
-        if (list.contains(val)) {
-            return false;
-        }
-        list.add(val);
-        return true;
+        
+		 if(map.containsKey(val)) {
+			 return false;
+		 }
+		 map.put(val, list.size());
+		 list.add(val);
+		return true;
+	        
+	    
     }
-
+    
     public boolean remove(int val) {
-        if (list.contains(val)) {
-            list.remove(Integer.valueOf(val));
-            return true;
-        }
-        return false;
+        
+	    	if(!map.containsKey(val)) {
+	    		return false;
+	    	}
+	    	
+	    	int idx=map.get(val);
+	    	int lastIdx=list.size()-1;
+	    	
+	    	if(idx!=lastIdx) {
+	    		list.set(idx, list.get(lastIdx));
+	    		map.put(list.get(lastIdx), idx);
+	    	}
+	    	map.remove(val);
+	    	list.remove(lastIdx);
+			return true;
+	        
+	    
     }
-
+    
     public int getRandom() {
-        int randomIdx = (int) (Math.random() * list.size());
-        return list.get(randomIdx);
+        
+			return list.get(random.nextInt(list.size()));
+	        
+	    
+        
     }
 }
-
