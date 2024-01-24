18021a89e09e6dd4c4fd4a042632c6460497009a
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2024-01-24 18:17:28 +0530

    Time: 11 ms (77.94%) | Memory: 70.2 MB (46.97%) - LeetSync

---
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int pseudoPalindromicPaths (TreeNode root) {
+        return parsePalinTree(root, new int[10]);
+    }
+    
+    public int parsePalinTree(TreeNode root, int[] arr){
+        
+        int count = 0; 
+        arr[root.val]++;
+        //Base Case: Reach a leaf node
+        if(root.left == null && root.right == null){
+            boolean isValidPath = isPalinDromePath(arr);
+            if(isValidPath) count = 1;
+        }
+        // Left and right subtree: 
+        if(root.left != null) count += parsePalinTree(root.left, arr);
+        if(root.right != null) count += parsePalinTree(root.right, arr);
+            
+        arr[root.val]--; 
+        return count; 
+    }
+    
+    public boolean isPalinDromePath(int[] arr){
+        boolean firstOdd = true;
+        int len = arr.length;
+        for(int i = 0; i < len; i++){
+            if(arr[i] % 2 == 1){
+                if(!firstOdd) return false;
+                firstOdd = false;
+            }
+        }
+        return true;
+    }
+}
