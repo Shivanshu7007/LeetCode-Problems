45b240302babd7dd937fec5078a36e8195299950
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2024-01-28 21:48:43 +0530

    Time: 80 ms (98.84%) | Memory: 43.7 MB (96.24%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.java
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.java
@@ -1,33 +1,31 @@
-
-
 class Solution {
-    public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        for (int row = 0; row < m; row++) {
-            for (int col = 1; col < n; col++) {
-                matrix[row][col] += matrix[row][col - 1];
-            }
-        }
-
-        int count = 0;
-
-        for (int c1 = 0; c1 < n; c1++) {
-            for (int c2 = c1; c2 < n; c2++) {
-                Map<Integer, Integer> map = new HashMap<>();
-                map.put(0, 1);
-                int sum = 0;
-
-                for (int row = 0; row < m; row++) {
-                    sum += matrix[row][c2] - (c1 > 0 ? matrix[row][c1 - 1] : 0);
-                    count += map.getOrDefault(sum - target, 0);
-                    map.put(sum, map.getOrDefault(sum, 0) + 1);
+    public int numSubmatrixSumTarget(int[][] nums, int t) {
+        int row=nums.length;
+        int col=nums[0].length;
+        int count=0;
+        for(int i=0;i<row;i++)//1
+        {
+            int arr[]=new int[col];
+            for(int j=i;j<row;j++)//2
+            {
+                for(int k=0;k<col;k++)//3
+                {
+                    arr[k]+=nums[j][k];
+                }
+                for(int l=0;l<col;l++)//4
+                {
+                    int sum=0;
+                    for(int r=l;r<col;r++)//5
+                    {
+                        sum+=arr[r];
+                        if(sum==t)
+                        {
+                            count++;
+                        }
+                    }
                 }
             }
         }
-
         return count;
     }
 }
-
