dffe3717e612bc0c262b8b85a525fe81e79ef241
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2024-02-09 12:42:49 +0530

    Time: 13 ms (99.83%) | Memory: 43.4 MB (19.11%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -1,33 +1,34 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,-1);
         Arrays.sort(nums);
-        
-        int maxSize = 1, maxIndex = 0;
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    if (dp[i] > maxSize) {
-                        maxSize = dp[i];
-                        maxIndex = i;
-                    }
+        int[] hash = new int[nums.length];
+        int maxi = -1;
+        int lastIndex = 0;
+        for(int index=0;index<nums.length;index++){
+            hash[index] = index;
+            for(int col=0;col<index;col++){
+                
+                if(nums[index]%nums[col]==0 && 1 + dp[col] > dp[index]){
+                    dp[index] = 1 + dp[col];
+                    hash[index] = col;
                 }
             }
-        }
-        
-        List<Integer> result = new ArrayList<>();
-        int num = nums[maxIndex];
-        for (int i = maxIndex; i >= 0; i--) {
-            if (num % nums[i] == 0 && dp[i] == maxSize) {
-                result.add(nums[i]);
-                num = nums[i];
-                maxSize--;
+            if(dp[index]>maxi){
+                maxi = dp[index];
+                lastIndex = index;
             }
+        } 
+        List<Integer> lst = new ArrayList<Integer>();
+        lst.add(nums[lastIndex]);
+        while(hash[lastIndex]!=lastIndex){
+            lastIndex = hash[lastIndex];
+            lst.add(nums[lastIndex]);
+            
+
         }
-        
-        return result;
+        return lst;
     }
+    
 }
