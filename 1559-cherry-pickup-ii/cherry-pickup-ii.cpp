3de90059c08863465f1b2d32ccfcf4605917a45c
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2024-02-11 21:15:06 +0530

    Time: 10 ms (100.00%) | Memory: 12.3 MB (80.55%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-        //m=number of rows and n = number of columns
-        int m = (int)grid.size(), n = (grid[0].size());
-        //initializing m and n
-        vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(n, vector<int>(n, 0)));
-        //dp dimensions
-        dp[0][0][n - 1] = 0 == n - 1 ? grid[0][0] : grid[0][0] + grid[0][n - 1];
-        //row traversing
-        for (int row = 1; row < m; row++) {
-            //avoiding unnecessary column computations
-            for (int c1 = 0; c1 < min(n, row + 1); c1++) {
-                for (int c2 = max(0, n - 1 - row); c2 < n; c2++) {
-                    int prevMax = 0;
-                    //Moves of robos in columns
-                    for (int offset1 = max(0, c1 - 1); offset1 <= min(n - 1, c1 + 1); offset1++) {
-                        for (int offset2 = max(0, c2 - 1); offset2 <= min(n - 1, c2 + 1); offset2++) {
-                                prevMax = max(prevMax, dp[row - 1][offset1][offset2]); //9 states wediscussed can be summariazed 
-                        }
+        int n=grid.size();
+        int m=grid[0].size();
+        int dp[n][m][m];
+        memset(dp,0,sizeof(dp));
+        int ans=0;
+        dp[0][0][m-1]=grid[0][0]+grid[0][m-1];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                for(int k=0;k<m;k++){
+                    if(j>i || k<m-i-1) continue;
+                    if(j>k) continue;
+                    dp[i][j][k]=dp[i-1][j][k];
+                    if(j-1>=0){
+                        dp[i][j][k]=max(dp[i][j][k],dp[i-1][j-1][k]);
+                        if(k-1>=0) dp[i][j][k]=max(dp[i][j][k],dp[i-1][j-1][k-1]);
+                        if(k+1<m) dp[i][j][k]=max(dp[i][j][k],dp[i-1][j-1][k+1]);
                     }
-                    //Robo collision condition
-                    if (c1 == c2) dp[row][c1][c2] = prevMax + grid[row][c1];
-                    else dp[row][c1][c2] = prevMax + grid[row][c1] + grid[row][c2];
+                    if(j+1<m){
+                        dp[i][j][k]=max(dp[i][j][k],dp[i-1][j+1][k]);
+                        if(k-1>=0) dp[i][j][k]=max(dp[i][j][k],dp[i-1][j+1][k-1]);
+                        if(k+1<m) dp[i][j][k]=max(dp[i][j][k],dp[i-1][j+1][k+1]);                        
+                    }
+                    if(k-1>=0) dp[i][j][k]=max(dp[i][j][k],dp[i-1][j][k-1]);
+                    if(k+1<m) dp[i][j][k]=max(dp[i][j][k],dp[i-1][j][k+1]);
+                    if(j!=k){
+                        dp[i][j][k]+=(grid[i][j]+grid[i][k]);
+                    }else{
+                        dp[i][j][k]+=grid[i][j];
+                    }
+                    ans=max(ans,dp[i][j][k]);
                 }
             }
         }
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                res = max(res, dp[m - 1][i][j]);    
-            }
-        }
-        return res;
+        return ans;
     }
 };
