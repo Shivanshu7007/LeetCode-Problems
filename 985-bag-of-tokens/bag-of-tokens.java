4d6dc3889aff055a4f50a5be42aed860893c6095
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2024-03-04 17:47:36 +0530

    Time: 1 ms (100.00%) | Memory: 42.1 MB (99.68%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.java
+++ 985-bag-of-tokens/bag-of-tokens.java
@@ -1,44 +1,47 @@
-// Time Complexity: O(NlogN) (O(NlogN) for sorting & O(N) for two pointers.)
-// Space Complexity: O(logN)
-// where N is the number of tokens
 class Solution {
     public int bagOfTokensScore(int[] tokens, int power) {
-        // play i-th token face up -> lose tokens[i] power -> choose the smallest one
-        // play i-th token face down -> gain tokens[i] power -> choose the largest one
-        // hence, sort tokens first
-        Arrays.sort(tokens);
-        // two pointes - l for tracking face up & r for tracking face down
-        int l = 0, r = tokens.length - 1;
-        int cur_score = 0, mx_score = 0;
-        while (l <= r) {
-            // there are 3 cases
-            if (tokens[l] <= power) {
-                // case 1. play l-th tokens face up if its power <= the current power
-                // ---
-                // losing tokens[l] power
-                power -= tokens[l];
-                // and gaining 1 score
-                cur_score += 1;
-                // cur_score can be mx_score potentially
-                mx_score = Math.max(mx_score, cur_score);
-                // move the pointer to the right
-                l += 1;
-            } else if (cur_score >= 1) {
-                // case 2. play r-th tokens face down if the current score is at least 1
-                // ---
-                // gaining tokens[r] power
-                power += tokens[r];
-                // and losing 1 score
-                cur_score -= 1;
-                // move the pointer to the left
-                r -= 1;
-            } else {
-                // case 3. impossible to play
-                // ---
-                // either you don't enough power or enough score
-                break;
+        int score = 0,last=tokens.length,ans=0,i=0;
+        quicksort(tokens,0,last-1);
+        while(i<last && (power >= tokens[i] || score > 0)){
+            if(power >= tokens[i]){
+                power -= tokens[i];
+                score++;
+                i++;
+            }
+            else{
+                score--;
+                power += tokens[--last];
+            }
+            ans = Math.max(score,ans);
+        }
+        return ans;
+    }
+    private void quicksort(int[] arr, int left, int right) 
+    {
+        if (left < right) 
+        {
+            int pivotIndex = partition(arr, left, right);
+            quicksort(arr, left, pivotIndex - 1);
+            quicksort(arr, pivotIndex + 1, right);
+        }
+    }
+    private int partition(int[] arr, int left, int right) 
+    {
+        int pivotValue = arr[right];
+        int i = left - 1;
+        for (int j = left; j < right; j++) 
+        {
+            if (arr[j] < pivotValue) 
+            {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
-        return mx_score;
-    }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[right];
+        arr[right] = temp;
+        return i + 1;
+    }   
 }
