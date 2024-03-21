41a1f65b79bde3ef3bbce0a70744ba09d41ae589
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2024-03-21 21:17:22 +0530

    Time: 2 ms (41.29%) | Memory: 47 MB (15.19%) - LeetSync

---
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        if(list1==null || list2==null) {
+            return null;
+        }
+        ListNode curr = list1;
+        ListNode start = list1;
+        ListNode end =list1;
+        int count = 0;
+
+        while(curr!=null) {
+            count++;
+            if(count==a) {
+                start = curr;
+            }
+            curr = curr.next;
+            if(count==b+1) {
+                end = curr;
+            }
+        }
+
+        curr = list2;
+        start.next = list2;
+        while(curr.next!=null) {
+            curr=curr.next;
+        }
+
+        curr.next = end;
+        return list1;
+    }
+}
